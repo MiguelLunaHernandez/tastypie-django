b5ebd4237f7b42b0d80ac61d41ea464872082c08
MiguelLunaHernandez <lunahernandez.miguel@gmail.com>
2014-09-10 17:13:18 -0500

    import file ModelResource

--- tastypie/urls.py
+++ tastypie/urls.py
@@ -1,3 +1,4 @@
+"""
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
@@ -9,4 +10,16 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+)
+
+"""
+
+from django.conf.urls.defaults import *
+from apps.comentario.api import EntryResource
+
+entry_resource = EntryResource()
+
+urlpatterns = patterns('',
+	(r'^blog/', include('comentario.urls')),
+	(r'^api/', include ('entry_resource.urls')),
 )
